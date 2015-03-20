1fcf741a8c08de9aabe1a424639ea003cf843540
christopher lee <clee@edx.org>
2015-03-20 10:57:00 -0400

    MA-169 return requested val encoded video profiles
    MA-169 Mobile api now calls for mobile_low (default), mobile_high,
     and youtube profiles instead of only mobile_low.

--- lms/djangoapps/mobile_api/models.py
+++ lms/djangoapps/mobile_api/models.py
@@ -1,3 +1,21 @@
 """
 A models.py is required to make this an app (until we move to Django 1.7)
 """
+# pylint: disable=no-member
+
+from django.db.models.fields import TextField
+
+from config_models.models import ConfigurationModel
+
+
+class MobileApiConfig(ConfigurationModel):
+    """Configuration for the video upload feature."""
+    video_profiles = TextField(
+        blank=True,
+        help_text="A comma-separated list of names of profiles to include in video encoding downloads."
+    )
+
+    @classmethod
+    def get_video_profiles(cls):
+        """Get the list of profiles to include in the encoding download"""
+        return [profile for profile in cls.current().video_profiles.split(",") if profile]
